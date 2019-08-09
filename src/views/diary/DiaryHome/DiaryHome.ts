9d01716c8427ba4ed8e4bf829de9db9a27706727
Jaejong Heo <hapsoa@gmail.com>
2019-08-10 01:44:57 +0900

    update pagination

--- src/views/diary/DiaryHome/DiaryHome.ts
+++ src/views/diary/DiaryHome/DiaryHome.ts
@@ -15,31 +15,49 @@ export default class DiaryHome extends Vue {
       this.$store.commit('saveMethod', async () => {
         if (!_.isNil(this.$store.state.myUser)) {
           // this.diaries = await Diary.getByUserId(this.$store.state.myUser.data.uid);
-          await this.initRoute();
+          const initObject = await this.initRoute(5);
+          if (!_.isNil(initObject)) {
+            this.diaries = initObject.diaries;
+            this.pageLength = initObject.pageLength;
+          }
         }
       });
     } else if (!_.isNil(this.$store.state.myUser)) {
       // this.diaries = await Diary.getByUserId(this.$store.state.myUser.data.uid);
       // const lastDiary = await Diary.getLastDiary(this.$store.state.myUser.data.uid);
-      this.initRoute();
+      const initObject = await this.initRoute(5);
+      if (!_.isNil(initObject)) {
+        this.diaries = initObject.diaries;
+        this.pageLength = initObject.pageLength;
+      }
     } else {
       this.$router.push({ name: 'login' });
     }
   }
 
-  private async initRoute() {
+  private async initRoute(numOfDiariesPerPage: number): Promise<{
+    pageLength: number;
+    diaries: Diary[];
+  } | null> {
     // this.diaries = await Diary.getByUserId(this.$store.state.myUser.data.uid);
     try {
       const lastDiary = await Diary.getLastDiary(this.$store.state.myUser.data.uid);
-      console.log('lastDiary', lastDiary);
-      this.diaries = await Diary.getByPage({
+
+      const pageLength: number = Math.ceil(lastDiary.data.index / numOfDiariesPerPage);
+      const diaries = await Diary.getByPage({
         userId: this.$store.state.myUser.data.uid,
         pageNumber: 1,
         lastDiaryIndex: lastDiary.data.index,
-        numOfDiariesPerPage: 5
+        numOfDiariesPerPage
       });
+
+      return {
+        pageLength,
+        diaries
+      };
     } catch (error) {
       // diary가 아무것도 없을 때 에러날 수 있다.
+      return null;
     }
   }
 
